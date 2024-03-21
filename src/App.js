9f371ee0fbc2d520447e15a1b4383fc0eac047d6
LEEJEHEE <lunatic1702@gmail.com>
2024-03-21 14:56:21 +0900

    상단부 Skills 반응형 제작
    그 아래 포트폴리오 소개는 세로로 나열할거라 반응형 필요없음

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { useMediaQuery} from 'react-responsive';
+import { useMediaQuery } from 'react-responsive'
+ 
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
+}
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
+  return isTablet ? children : null
+}
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
+const Default = ({ children }) => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 })
+  return isNotMobile ? children : null
+}
+
 function App() {
-
-
 
 
   return (
@@ -14,6 +30,7 @@ function App() {
       <div className='top-nav'>
         <h3>이제희 PORTFOLIO.</h3>
       </div>
+      <Desktop>
       <div className='skills'>
         <span className='title'>Skills</span>
         <ul className='skill-list'>
@@ -46,11 +63,80 @@ function App() {
           </li>
         </ul>
       </div>
-
+      </Desktop>
+      <Mobile><div className='skills'>
+        <span className='title'>Skills</span>
+        <ul className='skill-list-mobile'>
+          <li>Back-end
+            <ul >
+              <li>JAVA</li>
+              <li>Spring Framework</li>
+              <li>Spring Boot</li>
+            </ul>
+          </li>
+          <li>Front-end
+            <ul>
+              <li>HTML5</li>
+              <li>css3</li>
+              <li>Javascript</li>
+              <li>react</li>
+            </ul>
+          </li>
+          <li>Database
+            <ul>
+              <li>Mysql</li>
+              <li>oracle</li>
+            </ul>
+          </li>
+          <li>ETC
+            <ul>
+              <li>AWS</li>
+              <li>Linux</li>
+            </ul>
+          </li>
+        </ul>
+      </div></Mobile>
+      
+    <Tablet>     <div className='skills'>
+        <span className='title'>Skills</span>
+        <ul className='skill-list'>
+          <li>Back-end
+            <ul >
+              <li>JAVA</li>
+              <li>Spring Framework</li>
+              <li>Spring Boot</li>
+            </ul>
+          </li>
+          <li>Front-end
+            <ul>
+              <li>HTML5</li>
+              <li>css3</li>
+              <li>Javascript</li>
+              <li>react</li>
+            </ul>
+          </li>
+          <li>Database
+            <ul>
+              <li>Mysql</li>
+              <li>oracle</li>
+            </ul>
+          </li>
+          <li>ETC
+            <ul>
+              <li>AWS</li>
+              <li>Linux</li>
+            </ul>
+          </li>
+        </ul>
+      </div></Tablet>
       <span>
         skill outs 요 아래에다 포트폴리오 자랑할거 쓸거임
       </span>
+      <div>
 
+
+    <Default>Not mobile (desktop or laptop or tablet)</Default>
+  </div>
     </div>
   );
 }
